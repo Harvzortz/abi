5fc4dd5d2c6bfb7750ef7b11b77f3bdcd0a29df9
Harvy <harvy.ortega@lorma.edu>
2021-04-21 14:17:35 +0800

    Commit Done

--- resources/js/Pages/Stocks/Index.vue
+++ resources/js/Pages/Stocks/Index.vue
@@ -10,6 +10,14 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
+            <div class="flex flex-row justify-end mr-28">
+              <inertia-link :href="route('stock.create')">
+                <button
+                  class="mx-2 my-2 bg-indigo-700 w-25 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-5 py-2 text-xs"
+                >
+                  Create
+                </button>
+              </inertia-link>
             <table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
   <tr class="text-left border-b-2 border-gray-300">
     <th class="px-4 py-3">Id</th>
@@ -19,10 +27,10 @@
     <th class="px-4 py-3">Barcode</th>
     <th class="px-4 py-3">Discontinued</th>
     <th class="px-4 py-3">Photo Path</th>
-    <th class="px-4 py-3">Date Created</th>
+    
   </tr>
   
-  <tr v-for="item in stocks" class="bg-gray-100 border-b border-gray-200">
+  <tr v-for="item in stocks" :key="item.id" class="bg-gray-100 border-b border-gray-200">
     <td class="px-4 py-3">{{item.id}}</td>
     <td class="px-4 py-3">{{item.description}}</td>
     <td class="px-4 py-3">{{item.stock_category_id}}</td>
@@ -30,7 +38,7 @@
     <td class="px-4 py-3">{{item.barcode}}</td>
     <td class="px-4 py-3">{{item.discontinued}}</td>
     <td class="px-4 py-3">{{item.photo_path}}</td>
-    <td class="px-4 py-3">{{item.created_at}}</td>
+    
   </tr> 
 
 
@@ -39,6 +47,7 @@
           </div>
         </div>
       </div>
+    </div>
     </div>
   </breeze-authenticated-layout>
 </template>
